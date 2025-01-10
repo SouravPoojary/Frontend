473350035096395128b2fd6e07e42298626e0fb3
SouravPoojary <souravpoojary10@gmail.com>
2025-01-10 14:45:17 +0530

    a

---
+++ src/Components/NavbarSC/AddService.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import '../Styles/ServiceCenter/ServiceForm.css';
+const AddService = ({ hideForm }) => {
+  const [formData, setFormData] = useState({
+    serviceName: '',
+    serviceDescription: '',
+    serviceCategory: '',
+    deliveryTime: '',
+    price:''
+   })
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        hideForm();
+    }
+  return (
+    <div className="form">
+      <h2>Add Service</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Service Name</label>
+        <input type="text" />
+        <label>Service Description</label>
+        <input type="text" />
+        <label>Service Categoty</label>
+        <input type="text" />
+        <label>Delivery Time</label>
+        <input type="text" />
+        <label>Price</label>
+              <input type="text" />
+              <button type='submit'>Add Service</button>
+      </form>
+    </div>
+  );
+}
+export default AddService;
